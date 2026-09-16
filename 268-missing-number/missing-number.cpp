51662a80ceac676b9849fec1e964c883e3fcdbae
Suborajeet Das <subrodazz@gmail.com>
2026-09-16 10:21:03 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (12.15%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,17 +2,40 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {//TC : O(n) && SC : O(n)
         int n = nums.size();
-        unordered_map<int, int> mpp;
-
-        for(auto it : nums){//O(n)
-          mpp[it]++;
+        int sum = (n*(n+1))/2;
+        int currSum = 0;
+        for(auto it: nums){
+            currSum += it;
         }
+        return sum-currSum;
+        // bool skip = false;
 
-        for(int i=0; i<=n; i++){//O(n)
-          if(mpp.find(i) == mpp.end()){
-            return i;
-          }
-        }
-        return 0;
+        // for(int i=0; i<n;i++){
+        //   if(abs(nums[i]) == n){
+        //     continue;
+        //   }else if(nums[abs(nums[i])] == 0){
+        //     skip = true;
+        //   }else{
+        //     nums[nums[i]] *= -1;
+        //   } 
+        // }
+
+        // for(int i=0; i<n;i++){
+        //   if(nums[i] == 0 && !skip) return i;
+        //   if(nums[i] > 0) return i;
+        // }
+        // return n;
+        // // unordered_map<int, int> mpp;
+
+        // // for(auto it : nums){//O(n)
+        // //   mpp[it]++;
+        // // }
+
+        // // for(int i=0; i<=n; i++){//O(n)
+        // //   if(mpp.find(i) == mpp.end()){
+        // //     return i;
+        // //   }
+        // // }
+        // // return 0;
     }
 };
